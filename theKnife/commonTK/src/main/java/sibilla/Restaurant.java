337b75eda6497b4368071acaa22f9e7f0378a904
Ginevra Sibilla <gasibilla@studenti.uninsubria.it>
2026-06-08 11:55:45 +0200

    docs(Sibilla): adding javadoc to the Restaurant class

--- theKnife/commonTK/src/main/java/sibilla/Restaurant.java
+++ theKnife/commonTK/src/main/java/sibilla/Restaurant.java
@@ -1,3 +1,8 @@
+/**
+ * @author Sibilla
+ * @version 1.0
+ */
+
 package sibilla;
 
 import java.io.Serial;
@@ -6,7 +11,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-/* un ristorante ha: id, nome, tipo cucina, array Utenti (owners), array di Sedi*/
+/**
+ * Restaurant model, entity of a restaurant
+ */
 public class Restaurant implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
@@ -16,6 +23,9 @@ public class Restaurant implements Serializable {
     private List<User> owners;
     private List<Location> locations;
 
+    /**
+     * An empty constructor for a restaurant. Nothing is passed to it.
+     */
     public Restaurant(){
          id = UUID.randomUUID().toString();
          name = "";
@@ -24,6 +34,14 @@ public class Restaurant implements Serializable {
          locations = new ArrayList<>();
     }
 
+    /**
+     * Declares a restaurant.
+     * @param id        restaurant's id
+     * @param name      restaurant's name
+     * @param cuisine   restaurant's type of cuisine
+     * @param owners    list of owners
+     * @param locations list of various locations of a restaurant
+     */
     public Restaurant(String id, String name, Cuisine cuisine, List<User> owners, List<Location> locations){
         this.id = id;
         this.name = name;
@@ -32,6 +50,14 @@ public class Restaurant implements Serializable {
         this.locations = new ArrayList<>(locations);
     }
 
+    /**
+     * Declares a restaurant with only one owner
+     * @param id        restaurant's id
+     * @param name      restaurant's name
+     * @param cuisine   restaurant's type of cuisine
+     * @param owner     initializes the list and adds owner
+     * @param locations list of locations
+     */
     public Restaurant(String id, String name, Cuisine cuisine, User owner, List<Location> locations){
         this.id = id;
         this.name = name;
@@ -41,42 +67,82 @@ public class Restaurant implements Serializable {
         this.locations = new ArrayList<>();
     }
 
+    /**
+     * getId
+     * @return returns id
+     */
     public String getId() {
         return id;
     }
 
+    /**
+     * setId
+     * @param id sets id
+     */
     public void setId(String id) {
         this.id = id;
     }
 
+    /**
+     * getName
+     * @return returns name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * setName
+     * @param name sets name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * getCuisine
+     * @return returns cuisine
+     */
     public Cuisine getCuisine() {
         return cuisine;
     }
 
+    /**
+     * setCuisine
+     * @param cuisine sets cuisine
+     */
     public void setCuisine(Cuisine cuisine) {
         this.cuisine = cuisine;
     }
 
+    /**
+     * getOwners
+     * @return returns owners
+     */
     public List<User> getOwners() {
         return owners;
     }
 
+    /**
+     * setOwners
+     * @param owners sets owners
+     */
     public void setOwners(List<User> owners) {
         this.owners = owners;
     }
 
+    /**
+     * getLocations
+     * @return returns locations
+     */
     public List<Location> getLocations() {
         return locations;
     }
 
+    /**
+     * setLocations
+     * @param locations sets locations
+     */
     public void setLocations(List<Location> locations) {
         this.locations = locations;
     }

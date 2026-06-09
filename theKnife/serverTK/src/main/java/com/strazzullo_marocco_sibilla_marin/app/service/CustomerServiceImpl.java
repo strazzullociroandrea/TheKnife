6eafe05ea8e9ed35838be8b8f128bfc5f25595d5
fapakslapa <stefanomarocco0@gmail.com>
2026-06-09 10:59:26 +0200

    feat(marocco): implement CustomerServiceImpl search method

---
+++ theKnife/serverTK/src/main/java/com/strazzullo_marocco_sibilla_marin/app/service/CustomerServiceImpl.java
@@ -0,0 +1,79 @@
+package com.strazzullo_marocco_sibilla_marin.app.service;
+
+import com.strazzullo_marocco_sibilla_marin.app.dao.LocationDAO;
+import com.strazzullo_marocco_sibilla_marin.app.dao.impl.LocationDAOImpl;
+import marocco.SearchFilter;
+import com.strazzullo_marocco_sibilla_marin.app.remote.CustomerService;
+import sibilla.Location;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ * Service implementation of {@link CustomerService} RMI remote interface.
+ * Exposes the location searching and filtering capability to RMI clients.
+ * Decouples logic from database access by querying the database through the {@link LocationDAO} interface.
+ * Extends {@link UnicastRemoteObject} to participate in RMI remote runtime communication.
+ *
+ * @version 1.0
+ * @Author Strazzullo Ciro Andrea, 763603, VA
+ * @Author Marocco Stefano, 762192, VA - author of this file
+ * @Author Sibilla Ginevra, 761114, VA
+ * @Author Marin Marco, 760622, VA
+ */
+public class CustomerServiceImpl extends UnicastRemoteObject implements CustomerService {
+    /**
+     * Unique identifier for serialization to ensure that a loaded class corresponds
+     * exactly to the serialized object.
+     */
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * The location DAO instance.
+     */
+    private final LocationDAO locationDAO;
+
+    /**
+     * CustomerServiceImpl constructor. Exports the remote object and initializes the DAO layer.
+     *
+     * @throws RemoteException if RMI export fails
+     */
+    public CustomerServiceImpl() throws RemoteException {
+        super();
+        this.locationDAO = new LocationDAOImpl();
+    }
+
+    /**
+     * CustomerServiceImpl constructor with custom DAO injected.
+     *
+     * @param locationDAO the location DAO implementation to use
+     * @throws RemoteException if RMI export fails
+     */
+    public CustomerServiceImpl(LocationDAO locationDAO) throws RemoteException {
+        super();
+        this.locationDAO = locationDAO;
+    }
+
+    /**
+     * Function to search for restaurant locations based on a search filter.
+     *
+     * @param filter the search criteria including cuisines, prices, location, and distance
+     * @return a list of locations matching the filter criteria
+     * @throws RemoteException if a remote communication error occurs
+     */
+    @Override
+    public List<Location> searchLocations(SearchFilter filter) throws RemoteException {
+        try {
+            if (filter == null) {
+                filter = new SearchFilter.Builder().build();
+            }
+            System.out.println("Executing location search using filter parameters: " + filter);
+            return locationDAO.search(filter);
+        } catch (SQLException e) {
+            System.err.println("SQLException encountered in searchLocations service method: " + e.getMessage());
+            throw new RemoteException("Database access exception occurred on the server side.", e);
+        }
+    }
+}
